f2a71d2fddaa28580039fa6e7bafa17ed667ebac
kgs101 <prasadthorve0@gmail.com>
2024-02-26 13:04:43 +0530

    merge all

--- Backend/EJS/project1.0/index.js
+++ Backend/EJS/project1.0/index.js
@@ -9,8 +9,20 @@ let day = d.getDay();
 // console.log(d);
 console.log(day);
 
+let type = " a weekday";
+let action = "work hard!";
+
+if(day==0 || day==6 ){
+    type = "a weekend";
+    action = "have fun!";
+}
+
 app.get("/",(req,res)=>{
-    res.render("index.ejs")
+    res.render("index.ejs",{
+        daytype : type, // weekday
+        activity : action, //work hard
+    })
+
 });
 
 app.listen(port,()=>{
